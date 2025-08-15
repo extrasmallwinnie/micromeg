2d3e34a459d446c22486c97aaa604efde1268fb7
extrasmallwinnie <hoxygene@gmail.com>
2025-08-15 15:14:38 -0500

    added example ASV table

---
+++ R/calcBetaDiv.R
@@ -0,0 +1,25 @@
+#' Calculate beta diversity dissimilarities with or without rarefaction
+#'
+#' @param asvtable Input ASV table. Can be filtered with filtering()
+#' @param numRare Number of rarefactions to perform. Set to 0 if you do not want to do any rarefication.
+#' @param method Dissimilarity index to use. See ?vegan::vegdist for more information.
+#'
+#' @returns A dissimilarity matrix
+#' @export
+#'
+#' @examples
+#' asvtable <- makeExampleSeqtab()
+#' calcBetaDiv(asvtable, 400, "bray")
+calcBetaDiv <- function(asvtable, numRare = 400, method="bray"){
+
+  minlibrary<-min(rowSums(asvtable[-1]))
+
+  if(numRare>0) {
+    rarefactions <- lapply(as.list(1:numRare), function(x) vegan::rrarefy(asvtable[-1], minlibrary))
+    braycurtis <- lapply(rarefactions, function(x) vegan::vegdist(x, method=method, binary=TRUE))
+    averagedbc<-Reduce("+", braycurtis) / length(braycurtis)
+  } else {
+    averagedbc <- vegan::vegdist(asvtable[-1], method=method, binary=TRUE)
+  }
+
+}
