11defdc22af896563c2a9a8cdc8731b7be527c2b
extrasmallwinnie <hoxygene@gmail.com>
2025-08-15 12:24:09 -0500

    Added more tests for checkMeta

---
+++ R/filtering.R
@@ -0,0 +1,15 @@
+#' Filter the ASV table
+#'
+#' @param seqtab Input ASV table
+#' @param minDepth Minimum number of reads a sample must have to be kept
+#' @param minASVCount Minimum count an ASV must have to be kept
+#'
+#' @returns A filtered ASV table
+#' @export
+#' @import dplyr
+#' @import magrittr
+#'
+
+filtering <- function(seqtab, minDepth, minASVCount){
+  seqtab %>% dplyr::rowwise() %>% dplyr::mutate("row.sum" = sum(dplyr::c_across(where(is.numeric)))) %>% dplyr::filter(seqtab$row.sum > minDepth) %>% dplyr::select(-c("row.sum")) %>% dplyr::select(c("X", where(~ is.numeric(.) && sum(.) >= minASVCount))) %>% dplyr::rename("SampleID" = "X")
+}
--- tests/testthat/test-checkMeta.R
+++ tests/testthat/test-checkMeta.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Get a warning if NAs are detected", {
+  expect_warning(checkMeta(dplyr::starwars %>% dplyr::select(-c(films, vehicles, starships)), "name"))
+})
+
+test_that("Get an error if your sample IDs are not all unique", {
+  expect_error(checkMeta(dplyr::storms, "name"))
+  })
+
+test_that("Get an error if the column name you gave for the sample IDs can't be found", {
+  expect_error(checkMeta(dplyr::storms, "nameee"))
+})
+
+test_that("No output if it passes all tests", {
+  expect_null(checkMeta(dplyr::band_members, "name"))
 })
