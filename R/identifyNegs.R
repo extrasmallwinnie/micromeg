2dab2537100feb97273cb94f0cbc4c8825a0aea9
extrasmallwinnie <hoxygene@gmail.com>
2025-08-18 16:18:17 -0500

    added simple function to find lab negative controls in metadata

---
+++ R/identifyNegs.R
@@ -0,0 +1,17 @@
+#' Add a column to metadata object which identifies lab negative controls
+#'
+#' @param df Your metadata object
+#' @param column_to_look_in What is the name of the column that identifies which controls are negative?
+#' @param how What pattern should be looked for in the column to find the negative controls in that column?
+#'
+#' @returns An updated metadata object with a column called isNeg identifying lab negative controls
+#' @export
+#'
+#' @examples
+#' metadata <- makeExampleMeta()
+#' identifyNegs(metadata, "SampleType", "negative control")
+identifyNegs <- function(df=NULL, column_to_look_in=NULL, how=NULL){
+  df$isNeg <- 0
+  df$isNeg[df[[column_to_look_in]]==how] <- 1
+  return(df)
+}
